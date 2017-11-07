57f374d15ed7126cda9be058b91b689efb28ff5c
mirzasb <mirzasbaig@nyu.edu>
2017-11-07 15:11:18 -0500

    New socket lesson

---
+++ public/js/index.js
@@ -0,0 +1,25 @@
+var socket = io();
+
+socket.on('connect', function() {
+  console.log('Connected to server');
+  // socket.emit('createEmail', {
+  //   to: 'jen@example.com',
+  //   text: 'Hey'
+  // });
+  // socket.emit('createMessage', {
+  //   to: 'jen2@example.com',
+  //   text: 'Hey there.'
+  // });
+});
+
+socket.on('disconnect', function() {
+  console.log('Disconnected from server.');
+});
+
+// socket.on('newEmail', function(email) {
+//   console.log("New email", email);
+// });
+
+socket.on('newMessage', function(message) {
+  console.log("New message", message);
+});
--- server/server.js
+++ server/server.js
@@ -13,10 +13,38 @@ var io = socketIO(server);
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
+
   console.log('New user connected.');
+
+  // socket.emit('newEmail', {
+  //   from: 'mike@example.com',
+  //   text: 'Hello.',
+  //   createAt: 123
+  // });
+
+  // socket.emit('newMessage', {
+  //   from: 'wade@example.com',
+  //   text: 'Hello there.',
+  //   createAt: (new Date()).toJSON()
+  // });
+
   socket.on('disconnect', () => {
     console.log('Disconnected from the client.');
   });
+
+  // socket.on('createEmail', (newEmail) => {
+  //   console.log('createEmail', newEmail);
+  // });
+
+  socket.on('createMessage', (newMessage) => {
+    console.log('createMessage', newMessage);
+    io.emit('newMessage', {
+      from: newMessage.from,
+      text: newMessage.text,
+      createdAt: new Date().getTime()
+    });
+  });
+
 });
 
 server.listen(port, () => {
